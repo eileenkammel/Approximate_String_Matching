7fe0bf0fdf951652a0131a4cd3bd6270ca4bdbdd
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-15 15:46:53 +0200

    add Levenshtein Distance Metric

---
+++ model/levenshtein.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Author: Eileen Kammel, 811770
+
+from abstract_metric import Metric
+
+
+class Levenshtein(Metric):
+    """Levenshtein Distance Metric."""
+
+    @staticmethod
+    def min_edit_dist(word1, word2):
+        """Calculates Levenshtein Distance of two words.
+
+        Comparing to words is case sensitve, so that e.g.
+        Bill and bill differ.
+
+        Args:
+            word1: str
+            word2: str
+
+        Returns:
+            int: Levenshtein Distance of two given words.
+
+        Raises:
+            SameWordError:
+        """
+        pass
