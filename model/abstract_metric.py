d3da920d034be70bbd826b4471e4a5a7a4cdf9ec
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-15 15:43:48 +0200

    add Abstract Class for the Metrics

---
+++ model/abstract_metric.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Author: Eileen Kammel, 811770
+
+from abc import ABC, abstractmethod
+
+
+class Metric(ABC):
+    """Abstract class for matching metrics."""
+
+    @staticmethod
+    @abstractmethod
+    def min_edit_dist(word1, word2):
+        """Calculates the similarity of two words.
+        Args:
+            word1: str
+            word2: str
+
+        Returns:
+            similarity value of metric appropriate type
+
+        Raises:
+            SameWordError:
+        """
+        pass
