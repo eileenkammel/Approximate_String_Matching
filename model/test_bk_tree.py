ec02322a5c33917e452346f7becafaa02b1c38bb
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-18 16:30:17 +0200

    add fixture, more tests, file header

--- model/test_bk_tree.py
+++ model/test_bk_tree.py
@@ -1,14 +1,30 @@
-from bk_tree import BKNode, BKTree
+# -*- coding: utf-8 -*-
+# Author: Eileen Kammel, 811770
+
+import pytest as pt
+from bk_tree import BKTree
+
+
+@pt.fixture(scope="session", autouse=True)
+def test_tree():
+    test_tree = BKTree()
+    test_tree.set_up_from_file("model/demo_wordlist.txt")
+    return test_tree
 
 
 class TestBK:
 
-    def test1(self):
-        test_tree = BKTree()
-        test_tree.set_up_from_file("model/demo_wordlist.txt")
+    def test1(self, test_tree):
         assert len(test_tree.tree_root.get_children_with_distance()) == 4
 
-    def testweight(self):
-        test_tree = BKTree()
-        test_tree.set_up_from_file("model/demo_wordlist.txt")
+    def testweight(self, test_tree):
         assert test_tree.tree_root.get_child_distances()[0] == 1
+
+    def testweight1(self, test_tree):
+        assert test_tree.tree_root.get_child_distances()[1] == 2
+
+    def testweight2(self, test_tree):
+        assert test_tree.tree_root.get_child_distances()[2] == 3
+
+    def testweight3(self, test_tree):
+        assert test_tree.tree_root.get_child_distances()[2] == 4
