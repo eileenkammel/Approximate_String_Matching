4f563341ad8a6b09be34f0b53b88f048e174cba3
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-24 18:41:59 +0200

    add more tests

--- model/test_sorensen_dice.py
+++ model/test_sorensen_dice.py
@@ -14,3 +14,10 @@ class TestSDC:
 
     def test_3(self):
         assert SorensenDiceCoefficient.min_edit_dist("night", "nacht") == 0.25
+
+    def test_4(self):
+        assert SorensenDiceCoefficient.min_edit_dist(
+            "schnittblumen", "schnitbluhmen") == 0.8333333333333334
+
+    def test_5(self):
+        assert SorensenDiceCoefficient.min_edit_dist("rhythmus", "rytmus") == 0.5
