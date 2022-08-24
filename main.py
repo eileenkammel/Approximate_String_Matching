ae8adb4c5c41a98fb27f1afb879aa5e1926187bf
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-24 13:48:10 +0200

    add saving

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from controller.controller import InteractiveMatcher
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-f", type=str, help="Path to either a .txt file containing a wordlist or a .pkl file containing a pre set-up and pickeled BK-Tree")
+parser.add_argument("-s", action="store_true")
 
 args = parser.parse_args()
 
 file = args.f
+save = args.s
 
 test = InteractiveMatcher()
 
-test.get_data(file)
+test.get_data(file, save)
 test.find_matches()
