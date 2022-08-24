1ba94d232f2ea4c116e22e92d9c44f49e810e60e
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-24 11:55:52 +0200

    add view

---
+++ view/view.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8 -*-
+# Author: Eileen Kammel, 811770
+
+
+class ApproxMatchViewer:
+
+    @staticmethod
+    def show_tree_stats(depth, words):
+        print(
+            f"A BK-Tree with depth {depth}, containing {words} unique words has been created.")
+
+    @staticmethod
+    def show_matches(query_word, max_dist, matches):
+        print(f"For {query_word} and maximum edit distance {max_dist} the matches are: ", " ".join(
+            matches))
