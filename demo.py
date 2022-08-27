ea6f211438b49648cc7427d49b043f2f78e721c8
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-27 13:51:27 +0200

    add executable demo

---
+++ demo.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8 -*-
+# Author: Eileen Kammel, 811770
+
+from controller.controller import InteractiveMatcher
+
+demo = InteractiveMatcher()
+demo.set_up_tree("demo_data/demo_tree.pkl", "Levenshtein")
+demo.find_matches()
