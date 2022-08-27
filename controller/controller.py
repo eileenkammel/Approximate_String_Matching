8f704170874f1d11de75a4078779bd405efcce20
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2022-08-27 17:13:06 +0200

    make attributes private, fix typos

--- controller/controller.py
+++ controller/controller.py
@@ -19,11 +19,11 @@ class InteractiveMatcher:
     Model output is passed on to ApproxMatchViewer.
     """
     def __init__(self):
-        self.view = ApproxMatchViewer
-        self.tree = BKTree()
+        self._view = ApproxMatchViewer
+        self._tree = BKTree()
 
     def set_up_tree(self, filepath: str, metric_name: str, save: bool = False):
-        """Inititializes the BK-Tree set-up.
+        """Inititialize the BK-Tree set-up.
 
         The Tree is either set-up new from a textfile or loaded from
         a pkl. file depending on the filepath passed. The metric class
@@ -38,40 +38,40 @@ class InteractiveMatcher:
         """
         metric = InteractiveMatcher.determine_metric(metric_name)
         if filepath.endswith(".txt"):
-            self.tree.set_up_from_file(filepath, metric)
+            self._tree.set_up_from_file(filepath, metric)
             if save:
-                self.tree.save_to_file()
+                self._tree.save_to_file()
         if filepath.endswith(".pkl"):
-            self.tree = BKTree.load_from_file(filepath)
-        depth, words = self.tree.get_tree_stats()
-        self.view.show_tree_stats(depth, words)
+            self._tree = BKTree.load_from_file(filepath)
+        depth, words = self._tree.get_tree_stats()
+        self._view.show_tree_stats(depth, words)
 
     def find_matches(self):
-        """Executes the interactive matching mode.
+        """Execute the interactive matching mode.
 
         User is asked for input of query word and edit distance
-        tolerance limit. That input is passes on to the model.
+        tolerance limit. That input is passed to the model.
         """
         while True:
             word = input("Query word: ")
             if word == "":
                 break
             max_dist = float(input("Max edit distance: "))
-            matches = self.tree.query(word, max_dist)
-            self.view.show_matches(word, max_dist, matches)
+            matches = self._tree.query(word, max_dist)
+            self._view.show_matches(word, max_dist, matches)
 
     @staticmethod
     def determine_metric(metric_name: str):
-        """Determines which metric class to load.
+        """Determine which metric class to load.
 
         Commandline argument string is translated to matching
-        metreics class.
+        metrics class.
 
         Args:
             metric_name: Name of one of the available metrics.
 
         Returns:
-            one of the Metric classes.
+            One of the Metric classes.
         """
         if metric_name == "Levenshtein":
             return Levenshtein
